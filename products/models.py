8204009360301f098ba73027c37b11fe78da1cce
Felipe Contreras <contreras.felipe905@gmail.com>
2023-04-25 16:47:58 -0400

    reescrito de metodo str

--- products/models.py
+++ products/models.py
@@ -5,3 +5,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0) #123456.77
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
