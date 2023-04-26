e84148ccad7f2143b6d7ef792307483e71a71edc
Felipe Contreras <contreras.felipe905@gmail.com>
2023-04-26 18:39:36 -0400

    creacion de detail view de productos

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from products.models import Product
 
@@ -14,4 +15,15 @@ class ProductListView(ListView):
         context['products'] = context['product_list']
 
         #print(context)
+        return context
+    
+class ProductDetailView(DetailView): # busqueda mediante id
+    model = Product
+    template_name = 'products/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+
+        print(context)
         return context
