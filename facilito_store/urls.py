95fc49e7f4cce36b0ec952ef9f8d7265892bf847
Felipe Contreras <contreras.felipe905@gmail.com>
2023-04-25 23:21:20 -0400

    cambio a Productlistview

--- facilito_store/urls.py
+++ facilito_store/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from products.views import ProductListView
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', ProductListView.as_view(), name='index'),
     path('usuarios/login', views.login_view, name='login'),
     path('usuarios/logout', views.logout_view, name='logout'),
     path('usuarios/registro', views.register, name='register'),
