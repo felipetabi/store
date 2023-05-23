d75d1d630802dd14c27be695a1b38e36c1357889
Felipe Contreras <contreras.felipe905@gmail.com>
2023-05-23 16:08:56 -0400

    slug automaticos

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
@@ -7,6 +8,9 @@ class Product(models.Model):
     slug = models.SlugField(null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args,**kwargs):
+        self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
