fc26c11c97bb7b421f3ae1db128d5f83301da1dd
Felipe Contreras <contreras.felipe905@gmail.com>
2023-06-01 19:41:58 -0400

    cambio de nombre de la clase

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Categories(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
