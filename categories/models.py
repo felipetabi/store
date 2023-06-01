0fa448bbe3cc8e8b422f3a7c713f898e32210589
Felipe Contreras <contreras.felipe905@gmail.com>
2023-06-01 19:31:10 -0400

    creacion de clase Categories

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    title = models.Charfield(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
