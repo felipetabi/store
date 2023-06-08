f9276ca132924a241852763eaa73b0086b54e456
Felipe Contreras <contreras.felipe905@gmail.com>
2023-06-07 23:37:25 -0400

    renombrar busqueda de product

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search', views.ProductSearchListView.as_view(), name='search'),
+    path('search', views.ProductSearchListView.as_view(), name='search_product'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),
 ]
