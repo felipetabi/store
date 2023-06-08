204ddd2646befc7f1f8856fb776757c786ce7f7c
Felipe Contreras <contreras.felipe905@gmail.com>
2023-06-07 23:46:27 -0400

    cambios en la busqueda de products

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
-    path('search', views.ProductSearchListView.as_view(), name='search_product'),
+    path('search', views.ProductSearchListView.as_view(), name='search'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),
 ]
