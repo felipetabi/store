df78ad8c505fbac8f30af1aa993eb47f762b688b
Felipe Contreras <contreras.felipe905@gmail.com>
2023-06-20 16:20:26 -0400

    implementacion de session

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
+from .models import Cart
+
 # Create your views here.
 def cart(request):
 
-    return render(request, 'carts/cart.html')
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+
+    if cart_id:
+        cart = Cart.objects.get(pk=cart_id) #Obtenemos el carrito de la base de datos
+    else:
+        cart = Cart.objects.create(user=user)
+
+    request.session['cart_id'] = cart.id
+
+    return render(request, 'carts/cart.html',{
+
+    })
