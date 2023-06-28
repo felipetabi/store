b7639e1c4550ecf7f503e089cf606e3046206e91
Felipe Contreras <contreras.felipe905@gmail.com>
2023-06-27 22:49:06 -0400

    implementacion para agregar productos al carro

--- carts/urls.py
+++ carts/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name= 'carts'
 
 urlpatterns = [
-    path('', views.cart, name='cart')
+    path('', views.cart, name='cart'),
+    path('agregar', views.add, name='add')
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from products.models import Product
+
 from .models import Cart
 from .utils import get_or_create_cart
 # Create your views here.
@@ -8,4 +10,14 @@ def cart(request):
 
     return render(request, 'carts/cart.html',{
 
+    })
+
+def add(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+    
+    cart.products.add(product)
+
+    return render(request, 'carts/add.html', {
+        'product': product
     })
